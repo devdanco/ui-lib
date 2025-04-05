9c7582faaccdcfcac4494da9390fee36811638ab
devdanco <devdanco@gmail.com>
2025-04-05 14:32:46 +0200

    lib folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "../dist/assets/vite-ui-lib.css";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Button } from "../";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <Button>Hello</Button>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { dirname, resolve, relative, extname } from "node:path";
+import { fileURLToPath } from "node:url";
+import dts from "vite-plugin-dts";
+import { glob } from "glob";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: resolve(__dirname, "tsconfig.lib.json"),
+      rollupTypes: true,
+    }),
+  ],
+  build: {
+    copyPublicDir: false,
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime", "react-dom"],
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob
+          .sync("lib/**/*.{ts,tsx}", {
+            ignore: ["lib/**/*.d.ts"],
+          })
+          .map((file) => [
+            // 1. The name of the entry point
+            // lib/nested/foo.js becomes nested/foo
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            // 2. The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+        globals: {
+          "react/jsx-runtime": "jsxRuntime",
+          react: "React",
+          "react-dom": "ReactDom",
+        },
+      },
+    },
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+  },
+});
